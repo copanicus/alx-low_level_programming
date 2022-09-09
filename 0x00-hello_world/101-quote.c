c39ed2ed7854c9738b5db138b9a8dce6d4be62b9
copanicus <udemenick@gmail.com>
2022-09-08 22:01:32 -0700

    Task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+
+
+/**
+* main - point of entry
+* Description: Dora Korpar piece of art
+* *
+* Return: 0
+
+
+
+int main(void)
+{
+char str2[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str2, 61);
+return(1);
+}
