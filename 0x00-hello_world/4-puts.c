1345dec2d100ad2c5d57aaae3c0851146a2e8eec
copanicus <udemenick@gmail.com>
2022-09-08 20:57:38 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
-*main -print the string in the print function
+* main -print the string in the print function
 *
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle);
+	puts(*\"Programming is like building a multilingual puzzle);
 	return (0);
 }
