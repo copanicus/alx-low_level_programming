62cdcce6124059e1129b0e7159dac483353b87f7
copanicus <udemenick@gmail.com>
2022-09-08 21:26:23 -0700

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+/**
+* main - point of entry
+*
+* Description: the main function
+* with proper grammar, put the outcome is a piece of art,
+* Return: 0
+*/
+
+
+
+int main(void)
+{
+	printf("with proper grammar, put the outcome is a piece of art,\n");
+	return (0);
+}
