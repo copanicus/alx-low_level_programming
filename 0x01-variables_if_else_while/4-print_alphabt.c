b8605adad55f163aaba662d66e1581eaae5b5715
copanicus <udemenick@gmail.com>
2022-09-09 21:20:39 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+* main - missing alphabets
+*
+* Description: print alphabts
+* this program is to print "alphabets with some missing letters
+* Return: 0 always
+*/
+
+
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e'; && c != 'q')
+		{
+			putchar (c);
+		}
+	}
+	putchar (c);
+	return (0);
+}
