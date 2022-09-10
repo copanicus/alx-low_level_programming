918a9e2353df9e4bd777f022260cad2358333a0c
copanicus <udemenick@gmail.com>
2022-09-09 21:38:14 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+* main - print numbers
+*
+* Description: using the miain function
+* this program is to print "single digit number of base 10
+* Return:0 always
+*/
+
+
+
+int main(void)
+{
+	int d;
+	
+	for (d = 0; d < 10; ++d)
+	{
+		printf("%d", d);
+	}
+	printf('\n');
+	returnu (0);
+}
