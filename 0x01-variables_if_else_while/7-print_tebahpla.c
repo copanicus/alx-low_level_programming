1e2cfe237e5ae4097f42aca94400e9718af9efbd
copanicus <udemenick@gmail.com>
2022-09-09 22:12:49 -0700

    Task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -15,9 +15,10 @@
 int main(void)
 
 {
-	char c; 	
-	for (c = 'z'; c >= 'a'; --c)
-		putchar(c);
-	putchar('\n');
-	return (0);
+char c; 	
+
+for (c = 'z'; c >= 'a'; --c)
+	putchar(c);
+putchar('\n');
+return (0);
 }
