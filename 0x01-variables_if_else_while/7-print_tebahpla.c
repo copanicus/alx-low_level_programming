039311fc7089660ab7c0b35cc94b21b09357338c
copanicus <udemenick@gmail.com>
2022-09-09 22:04:53 -0700

    Task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+/**
+* main - print in reverse order
+*
+* Description: in the main function
+* this program is to print "alphabets in the reverse order
+* Return: 0 always
+*/
+
+
+
+int main(void)
+
+{
+	char c; 
+	
+	for(c = 'z'; c >= 'a'; --c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
