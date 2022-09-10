0801727f694dd7e9cce9c96620426f88feff1255
copanicus <udemenick@gmail.com>
2022-09-09 22:26:02 -0700

    Task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+* main - print in base 16
+*
+* Return: 0 always 
+*/
+
+
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchat(i);
+	putchar('\n');
+	return (0);
+}
