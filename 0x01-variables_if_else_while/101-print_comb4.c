866629011d3900cb2b7975d74d0cc8c495424b96
copanicus <udemenick@gmail.com>
2022-09-11 06:02:26 -0700

    Task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+* main - print combination of 3 numbers
+*
+* Return: 0 Always
+*/
+
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar (a);
+					putchar (b);
+					putchar (c);
+					if (a != '6')
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
+
