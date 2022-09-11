6c61675bd5f8bd73ff351be63c9f476316a79805
copanicus <udemenick@gmail.com>
2022-09-11 05:39:26 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+* main - print numbers from 00 to 89
+*
+* Return: Always 0
+*/
+
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a < b)
+		{
+			putchar (a);
+			putchar (b);
+			if (a != '8' || (a == '8' && b != '9'))
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n')
+		return(0);
+}
+
