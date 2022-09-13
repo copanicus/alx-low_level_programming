768b5b0aa1dfe4b7ad894473e4843d475366a488
copanicus <udemenick@gmail.com>
2022-09-13 11:54:46 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print alphabet in lower case
+*/
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar ('\n');
+}
+
