67cd900cbeaae0385bb6896dde34f7a5dc59c480
copanicus <udemenick@gmail.com>
2022-09-13 13:42:21 -0700

    Task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs - return absolute numbers
+* @n: parameters of n
+* Return: numbers that are absolute
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
