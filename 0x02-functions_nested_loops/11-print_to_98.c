bb5dee029d59fba783c99c5ef33d8ec8945987ff
copanicus <udemenick@gmail.com>
2022-09-13 16:58:26 -0700

    Task100

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_to_98 - printing numbers from 1 to 98
+*
+* Return: 0 always
+* @n: argument parameter
+*/
+
+void print_to_98(int n)
+{
+	for (n = 1; n <= 98; n++)
+	{
+		printf("%d ", n);
+	}
+	printf('\n');
+	return (0);
+}
