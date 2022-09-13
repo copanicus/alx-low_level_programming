b2afafb125803a8e9115a788bd7b26622ac0277f
copanicus <udemenick@gmail.com>
2022-09-13 12:35:28 -0700

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isapha - to confirm alphabetic letters
+* 
+* @c: to check the characters of the letter
+* Return: depending on the conditions return 0 or 1
+*/
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c <= 'Z' && c >= 'A'))
+}
