8d12f0f52d668f3fff134a4603342994e0bc1f4f
copanicus <udemenick@gmail.com>
2022-09-13 17:27:22 -0700

    Task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,19 @@
 
 void print_to_98(int n)
 {
-	for (n = 1; n <= 98; n++)
+	if (n > 98)
 	{
-		printf("%d ", n);
+		for (n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
-	printf('\n');
-	return (0);
+	else if (n < 98)
+	{
+		for (n < 98; n++)
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("%d\n", n);
 }
