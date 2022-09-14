087ce7e17dc4241eaaea4ef156670d6602413b41
copanicus <udemenick@gmail.com>
2022-09-13 18:17:58 -0700

    Task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,21 +18,25 @@ void print_times_table(int n)
 			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
-				if (b == 0){
+				if (b == 0)
+				{
 					_putchar(c + '0');
-				}else if (c < 10 && b != 0){
+				}else if (c < 10 && b != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(c + '0');
-				}else if (c >= 10 && c < 100){
+				}else if (c >= 10 && c < 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
-				}else if (c >= 100){
+				}else if (c >= 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 100) + '0');
