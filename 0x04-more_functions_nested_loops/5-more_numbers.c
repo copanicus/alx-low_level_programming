ee6926336deb6730517c899f2cbed95ee71b427c
copanicus <udemenick@gmail.com>
2022-09-15 15:37:12 -0700

    Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - print number from 0 to 14
+*
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; j <= 14; b++)
+		{
+			if (a >= 10)
+				_putchar('1');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
