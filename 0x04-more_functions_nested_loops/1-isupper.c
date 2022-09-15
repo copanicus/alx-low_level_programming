dd4ab00508f65f3b7e8924fe33aa2a585b525719
copanicus <udemenick@gmail.com>
2022-09-15 12:59:33 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isupper - uppercase alphabets
+*
+* Description: printing of uppercase letters
+* @c: checking characters
+* Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	if (c != 'a' && c != 'z')
+		return (0);
+	else
+		return (1);
+}
