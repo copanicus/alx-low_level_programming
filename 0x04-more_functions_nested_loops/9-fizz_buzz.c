e5c0db6ea51ec6975adb284f5a40160002d79f43
copanicus <udemenick@gmail.com>
2022-09-15 17:34:17 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 15 == 0)
-			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", i);
 		i++;
