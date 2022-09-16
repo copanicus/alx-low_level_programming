cdccd90b5b9b090f17cd602062f0f6566584bbe0
copanicus <udemenick@gmail.com>
2022-09-15 18:43:26 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,38 +2,39 @@
 #include <stdio.h>
 
 /**
-* main - multiple of 3 Fizz while that of 5 Buzz
+* main - Fizz for multiple of 3 Buzz for 5
 *
-* Description: multiple of both FizzBuzz
+* Description: for both 3 and 5 FizzBuzz
 * Return: 0 always
 */
 
 int main(void)
 {
-int i;
-
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
 }
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else
-{
-printf(" %d", i);
-}
-}
-printf("\n");
-return (0);
