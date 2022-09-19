935e0549fe288672176d54180cc7058945c9cc71
copanicus <udemenick@gmail.com>
2022-09-19 13:08:30 -0700

    Task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - print element of n array
+* @a: array
+* @n: number of element
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	if (n > 0)
+	{
+		for (b = 0; b < n; b++)
+		{
+			if (b != n - 1)
+			{
+				printf("%d, ", a[b]);
+			}
+			else
+			{
+				printf("%d", a[b]);
+			}
+		}
+	}
+	printf('\n');
+}
