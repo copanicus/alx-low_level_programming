682e4cd0c63cb9ab86913fc48e3db357717afc7c
copanicus <udemenick@gmail.com>
2022-09-19 15:22:34 -0700

    Task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int j;
 
-	for (j = 0; src[j]; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
 	}
-	dest[j] = src[j];
+	dest[j++] = '\0';
 	return (dest);
 }
