3bc9ef16e4bc6e56f9d649c1c6b224ae14368dcd
copanicus <udemenick@gmail.com>
2022-09-19 06:17:32 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns a string lenght
+* @s: input param
+* Return: lenght
+*/
+
+int _strlen(char *s)
+{
+	int a;
+
+	while (s[a])
+	{
+		a++;
+	}
+	return (a);
+}
+
+
