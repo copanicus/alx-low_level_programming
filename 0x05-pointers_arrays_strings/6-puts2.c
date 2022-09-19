e0703d077e66bd6082209bf1a556d72752b8b801
copanicus <udemenick@gmail.com>
2022-09-19 12:16:15 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts2 - prints one a char from the 2 string
+* @str: string want to print
+* Description: and newline
+* Return: void
+*/
+
+void put2(char *str)
+{
+	int a = 0;
+	while (str[a] != '\0')
+	{
+		if (a % 2 ==  0)
+		{
+			_putchar(str[a]);
+			a++;
+		}
+		_putchar('\n');
+	}
+}
