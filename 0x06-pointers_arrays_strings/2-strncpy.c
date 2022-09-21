2dc16cc0d5b9aae9a539f065a1d0a28950b995f3
copanicus <udemenick@gmail.com>
2022-09-21 15:17:27 -0700

    Task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strncpy - copy string
+* @dest: destination
+* @src: source
+* @n: amount of byte in the src
+* Return: pointer to dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for ((a = 0; a < n) && (src[a] != '\0'; a++))
+		dest[a] = src[a];
+	for (; a < n; a++)
+		dest[a] = '\0';
+	return (dest);
+}
