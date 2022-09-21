36d128575a180e3907e99ef64cf7da11c92c12ef
copanicus <udemenick@gmail.com>
2022-09-21 15:02:30 -0700

    Task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,16 +13,17 @@ char _strncat(char *dest, char *src, int n)
 	int a = 0;
 	int b = 0;
 
-	while (dest[a] != '\0')
+	while (*(dest + a) != '\0')
 	{
-		++a;
+		a++;
 	}
-	while (src[b] != 0 && b < n)
+	while (b < n)
 	{
-		dest[a] = src[b];
-		++b;
-		++a;
+		*(dest + a) = *(src + b);
+		if (*(src + b) == '\0')
+			break;
+		b++;
+		a++;
 	}
-	dest[a] = '\0';
 	return (dest);
 }
