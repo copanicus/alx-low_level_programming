39190c995f798f2ac4769d4bb898f7ee4d240b8c
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-09-23 06:42:50 -0400

    Task13

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,23 +11,23 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int a, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
+	for (a = 0; n1[a]; a++)
 		;
 	for (j = 0; n2[j]; j++)
 		;
-	if (i > size_r || j > size_r)
+	if (a > size_r || j > size_r)
 		return (0);
 	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	for (a -= 1, j -= 1, k = 0; k < size_r - 1; a--, j--, k++)
 	{
 		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
+		if (a >= 0)
+			n += n1[a] - '0';
 		if (j >= 0)
 			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
+		if (a < 0 && j < 0 && n == 0)
 		{
 			break;
 		}
@@ -35,7 +35,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (a >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
