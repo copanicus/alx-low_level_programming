ccff7b86b7760fb03254e0e70a1fab587535afc1
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-10-02 21:34:19 -0400

    2

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _islower - if is a small character case
+*
+* Description: is the letter lowercase
+* @c: a character argument
+* Return: 1 or 0 depends on the condition
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
