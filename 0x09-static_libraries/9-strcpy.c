3daed23d06cf35c96954bef92447b6ec84d0fe8c
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-10-02 21:53:24 -0400

    8

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strcpy - copy and paste strings
+* @dest: value destination
+* @src: value source
+*
+* Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
