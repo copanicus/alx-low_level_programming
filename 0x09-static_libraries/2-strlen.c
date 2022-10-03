7b1aeb9ea0dfa757d12116a913d3c55acda0a238
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-10-02 21:49:07 -0400

    6

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - returns a string lenght
+* @s: input param
+* Return: lenght
+*/
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; ++a)
+	{
+		continue;
+	}
+	return (a);
+}
