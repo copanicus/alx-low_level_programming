028d6d7cc584b1b38e58d12e8a70d48078d1c535
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-10-02 21:35:43 -0400

    4

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isalpha - to confirm alphabetic letter
+*
+* Decription: is it an alphabet
+* @c: to display letters in small or big
+* Return: depending on the conditions return 0 or 1
+*/
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
