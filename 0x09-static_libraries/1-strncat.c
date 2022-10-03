b41f7d139f17d7ea08466a242c5fc5ef4900b8c3
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-10-02 21:58:47 -0400

    11

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strncat - concatenating two strings having n-bytes
+* @dest: destination
+* @src: source
+* @n: amount of bytes within the src
+* Return: pointer to dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(dest + a) != '\0')
+	{
+		a++;
+	}
+	while (b < n)
+	{
+		*(dest + a) = *(src + b);
+		if (*(src + b) == '\0')
+			break;
+		b++;
+		a++;
+	}
+	return (dest);
+}
