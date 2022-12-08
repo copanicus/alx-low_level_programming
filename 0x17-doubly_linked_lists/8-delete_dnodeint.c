b3d4744ffc44050bd98e01990202dd0218f35c8b
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-12-08 17:51:18 -0500

    next

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,6 @@
 /**
 * delete_dnodeint_at_index - deletes the node at index of a
 * dlistint_t linked list
-*
 * @head: head of the list
 * @index: index of the new node
 * Return: 1 if it succeeded, -1 if it failed
