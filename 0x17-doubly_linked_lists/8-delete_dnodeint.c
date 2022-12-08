ed18321bd4329d0c700ad9f9da4badb6d79910e8
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-12-08 18:00:33 -0500

    all

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,12 +8,12 @@
  * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h1;
 	dlistint_t *h2;
 	unsigned int i;
-
 	h1 = *head;
 
 	if (h1 != NULL)
