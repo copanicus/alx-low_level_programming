9814e25fad74e99fa2d70ce7d7ca651d3ed1c166
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2022-12-09 09:50:32 -0500

    having issues

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,48 +1,42 @@
-#include "list.h"
+#include "lists.h"
 
 /**
-* delete_dnodeint_at_index - deletes the node at index a
-* dlistint_t linked list
-* @head: head of the list
-* @index: index of the new node
-* Return 1 if sucessful an -1 if it fails
-*/
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-  dlistint_t *h1;
-  dlistint_t *h2;
-  unsigned int i;
-  
-  h1 = *head;
-  
-  if (h1 != NULL)
-    while (h1->prev != NULL)
-      h1 = h1->prev;
-  
-  i = 0;
-  
-  while (h1 != NULL)
-  {
-    if (i == index)
-    {
-      if (i == 0)
-      {
-        *head = h1->next;
-        if (*head != NULL)
-          (*head)->prev = NULL;
-      }
-      else
-      {
-        h2->next = h1->next;
-        if (h1->next != NULL)
-          h1->next->prev = h2;
-      }
-      free(h1);
-      return(1);
-    }
-    h2 = h1;
-    h1 = h1->next;
-    i++;
-  }
-  return(-1);
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
 }
